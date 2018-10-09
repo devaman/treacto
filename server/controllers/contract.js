8c71084ea901c6a0bf8f8d146341463f45218924
Amit Chambial <chambialamit@gmail.com>
2018-10-10 01:40:16 +0530

    Added Rinkeby Deployment

--- server/controllers/contract.js
+++ server/controllers/contract.js
@@ -1,11 +1,28 @@
+/*
+ * @Author: Amit Chambial 
+ * @Date: 2018-10-09 14:42:31 
+ * @Last Modified by: Amit Chambial
+ * @Last Modified time: 2018-10-10 01:38:12
+ */
 const contract = require('truffle-contract');
 const Web3 = require('web3');
-var provider = new Web3.providers.HttpProvider("http://localhost:8545");
+/* 
+Uncomment the below code to run local on ganache
+*/
+
+// var provider = new Web3.providers.HttpProvider("http://localhost:8545");
+
+/* 
+Uncomment the below code to run on rinkeby test net
+*/
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = "";
+var provider = new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/i1b6qEdLoWdspQ1ozvk4",0,10);
 const web3 = new Web3(provider);
 const simplestorage_artifact = require('../../build/contracts/SimpleStorage.json');
 var SimpleStorage = contract(simplestorage_artifact);
 SimpleStorage.setProvider(web3.currentProvider);
-SimpleStorage.defaults({ from: web3.eth.coinbase });
+// SimpleStorage.defaults({ from: web3.eth.coinbase });
 const self = {};
 const setAccounts = () => {
     web3.eth.getAccounts(function (err, accs) {
@@ -17,64 +34,67 @@ const setAccounts = () => {
             reject({ err: "Couldn't get any accounts! Make sure your Ethereum client is configured correctly." });
         }
         self.accounts = [...accs];
-        self.account = self.accounts[2];
-    })};
-    setAccounts();
-    module.exports = {
-        // start: () => new Promise((resolve, reject) => {
-        //     self.web3 = web3;
-           
-        //     self.web3.eth.getAccounts(function (err, accs) {
-        //         if (err != null) {
-        //             reject({ err: "There was an error fetching your accounts." });
-        //         }
+        self.account = self.accounts[0];
+        console.log(self.accounts);
 
-        //         if (accs.length == 0) {
-        //             reject({ err: "Couldn't get any accounts! Make sure your Ethereum client is configured correctly." });
-        //         }
-        //         self.accounts = [...accs];
-        //         self.account = self.accounts[2];
+    })
+};
+setAccounts();
+module.exports = {
+    // start: () => new Promise((resolve, reject) => {
+    //     self.web3 = web3;
 
-        //         resolve({ accounts: self.accounts });
-        //     });
-        // }),
-        set: (val) => new Promise((resolve, reject) => {
-            
-            var meta;
-            SimpleStorage.deployed().then(function (instance) {
-                meta = instance;
-                console.log(self.account);
+    //     self.web3.eth.getAccounts(function (err, accs) {
+    //         if (err != null) {
+    //             reject({ err: "There was an error fetching your accounts." });
+    //         }
 
-                meta.set(val, { from: self.account }).then(d => {
-                    console.log(d);
-                    resolve({ success: true })
-                }).catch(err => {
-                    console.log("meta err", err);
+    //         if (accs.length == 0) {
+    //             reject({ err: "Couldn't get any accounts! Make sure your Ethereum client is configured correctly." });
+    //         }
+    //         self.accounts = [...accs];
+    //         self.account = self.accounts[2];
 
-                })
+    //         resolve({ accounts: self.accounts });
+    //     });
+    // }),
+    set: (val) => new Promise((resolve, reject) => {
+
+        var meta;
+        SimpleStorage.deployed().then(function (instance) {
+            meta = instance;
+            console.log(self.account);
+
+            meta.set(val, { from: self.account }).then(d => {
+                console.log(d);
+                resolve({ success: true })
             }).catch(err => {
-                console.log("deployed err", err);
+                console.log("meta err", err);
 
             })
-
-        }),
-        get: () => new Promise((resolve, reject) => {
-            var meta;
-            SimpleStorage.deployed().then(function (instance) {
-                meta = instance;
-                console.log(self.account);
-
-                meta.get.call().then(d => {
-                    console.log(d);
-                    resolve({ success: true, val: d })
-                }).catch(err => {
-                    console.log("meta err", err);
-
-                })
-            }).catch(err => {
-                console.log("deployed err", err);
-
-            })
+        }).catch(err => {
+            console.log("deployed err", err);
 
         })
-    }
+
+    }),
+    get: () => new Promise((resolve, reject) => {
+        var meta;
+        SimpleStorage.deployed().then(function (instance) {
+            meta = instance;
+            console.log(self.account);
+
+            meta.get.call().then(d => {
+                console.log(d);
+                resolve({ success: true, val: d })
+            }).catch(err => {
+                console.log("meta err", err);
+
+            })
+        }).catch(err => {
+            console.log("deployed err", err);
+
+        })
+
+    })
+}
--- truffle.js
+++ truffle.js
@@ -1,4 +1,10 @@
 /*
+ * @Author: Amit Chambial 
+ * @Date: 2018-10-09 14:42:44 
+ * @Last Modified by: Amit Chambial
+ * @Last Modified time: 2018-10-10 01:09:00
+ */
+/*
  * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
@@ -11,7 +17,8 @@
  *     gasPrice: 10000000000,
  *   },
  */
-
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic ="";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -20,6 +27,12 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/i1b6qEdLoWdspQ1ozvk4");
+      },
+      network_id: 1
     }
   }
   
